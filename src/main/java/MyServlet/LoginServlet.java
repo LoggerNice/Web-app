8e16f000e694d2e05fcecc7420a6e7a04cca2049
LoggerNice <71228131+LoggerNice@users.noreply.github.com>
2022-03-13 16:02:14 +0400

    Auth

--- src/main/java/MyServlet/LoginServlet.java
+++ src/main/java/MyServlet/LoginServlet.java
@@ -3,6 +3,7 @@ package MyServlet;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.security.NoSuchAlgorithmException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -10,6 +11,7 @@ import java.sql.SQLException;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpSession;
 
 import org.junit.Test;
 
@@ -27,6 +29,7 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
     	request.setCharacterEncoding("utf-8");
         
+    	PrintWriter out = response.getWriter();
         String login = request.getParameter("Login");
         String password = request.getParameter("Password");
         ResultSet rs = null;
@@ -34,7 +37,7 @@ public class LoginServlet extends HttpServlet {
         try {
 			ConnectionDB connect = new ConnectionDB();
 			
-			String query = "SELECT login, password FROM users WHERE login = ?";
+			String query = "SELECT name, login, password FROM users WHERE login = ?";
 			PreparedStatement statement = connect.dbConnector().prepareStatement(query);
 			statement.setString(1, login);
 			rs = statement.executeQuery();
@@ -43,11 +46,26 @@ public class LoginServlet extends HttpServlet {
 				while (rs.next()) {
 					System.out.println(rs.getString("login") + " " + rs.getString("password"));
 					String hash = rs.getString("password");
+					String name = rs.getString("name");
 					
 					try {
 						String hashUser;
 						hashUser = RegistrationServlet.hushPass(password);
-						assertEquals("Password are not equal", hashUser, hash);
+						
+						if (hashUser.equals(hash)) {
+							HttpSession session = request.getSession();
+							session.setAttribute("Login", login);
+							session.setAttribute("Password", password);
+							
+							out.println("<h1>");
+					        out.println("Hello, " + name);
+					        out.println("</h1>");
+						}
+						else {
+							System.out.println("Password not equals!");
+							response.sendRedirect(request.getContextPath() + "/LoginForm.html");
+						}
+						
 					} catch (NoSuchAlgorithmException e) {
 						System.out.println("MD5 error!");
 						e.printStackTrace();
